838f9fb8b625b8dc396227712c9dc2bdf4f2f69a
13fengye <pengzhentong@aragoncs.com>
2023-07-03 14:05:13 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './pages/home.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+        {
+            test: /\.jsx?$/, // 匹配所有JSX文件
+            exclude: /node_modules/,
+            use: {
+            loader: 'babel-loader', // 使用babel-loader处理
+            options: {
+                presets: ['@babel/preset-react'], // 使用React预设
+            },
+            },
+        },
+        ],
+    },
+};
