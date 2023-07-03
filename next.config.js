7f2fcfff333729f15be86cca801893c55a1a419f
13fengye <pengzhentong@aragoncs.com>
2023-07-03 16:02:39 +0800

    UPD: set default page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/home', // 默认页面
+                permanent: true,
+            },
+        ];
+    },
+};
 
 module.exports = nextConfig
