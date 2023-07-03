2ed41a8b9645e8a876cb98a24126ff55caf814c7
13fengye <pengzhentong@aragoncs.com>
2023-07-03 15:20:29 +0800

    UPD: fix wepack error

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,15 @@
+import { useRouter } from "next/router"
+
 export default function Home() {
+  const router = useRouter();
+
+  const toOnePage = () => {
+    router.push('/one/');
+  }
   return (
-    <div>Welcome to Home page!</div>
+    <>
+      <div>Welcome to Home page!</div>
+      <button onClick={toOnePage}>第一页</button>
+    </>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,26 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-    },
+    }, 
     module: {
         rules: [
-        {
-            test: /\.jsx?$/, // 匹配所有JSX文件
-            exclude: /node_modules/,
-            use: {
-            loader: 'babel-loader', // 使用babel-loader处理
-            options: {
-                presets: ['@babel/preset-react'], // 使用React预设
+            {
+                test: /\.jsx?$/, // 匹配所有JSX文件
+                exclude: /node_modules/,
+                use: {
+                loader: 'babel-loader', // 使用babel-loader处理
+                    options: {
+                        presets: ['@babel/preset-react'], // 使用React预设
+                    },
+                },
             },
-            },
-        },
         ],
     },
+    resolve: {
+        fallback: {
+          fs: false,
+          stream: false,
+          zlib: false,
+        }
+    }
 };
